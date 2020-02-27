a97b24b56092345f71d7a82f40cf3bddfca04085
Varun Kumar <vakuma@microsoft.com>
2020-02-26 16:39:53 -0800

    Initial commit

---
+++ StaircaseSteps/CountStairsWays/CountStairsWays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CountStairsWays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] possibleSteps = { 1, 2};
+            int totalSteps = 4;
+            int[] result = new int[totalSteps];
+            printWays(totalSteps, result, possibleSteps, 0, 0);
+        }
+
+        private static void printWays(int totalSteps, int[] path, int[] possibilities, int pathIndex, int sumSoFar)
+        {
+            if(sumSoFar == totalSteps)
+            {
+                printPathUptoIndex(path, pathIndex);
+                return;
+            }
+
+            foreach(int p in possibilities)
+            {
+                if((sumSoFar + p) <= totalSteps)
+                {
+                    path[pathIndex] = p;
+                    printWays(totalSteps, path, possibilities, pathIndex + 1, sumSoFar + p);
+                }
+            }
+        }
+
+        private static void printPathUptoIndex(int[] path, int pathIndex)
+        {
+            for(int i = 0; i < pathIndex; i++)
+            {
+                Console.Write(path[i] + ",");
+            }
+            Console.WriteLine();
+        }
+    }
+}
